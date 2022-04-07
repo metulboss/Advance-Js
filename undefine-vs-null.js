bbc95c4bacce833c4859350f5abc36eda5163b34
metulboss <metulromel@gmail.com>
2022-04-07 23:19:10 +0600

    undefine vs null

---
+++ undefine-vs-null.js
@@ -0,0 +1,18 @@
+
+
+let name;
+
+function Test(num1, num2){
+ console.log(num1+num2);
+}
+
+function add(num1, num2){
+    console.log(num1,num2)
+}
+
+
+
+const pemikList = {name:"metul romel", phone:098888}
+console.log(pemikList.gf);
+
+
